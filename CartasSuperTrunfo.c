db96a1fccda3c94e70770e4faa63d5bc7085cea2
Luís Felipe Santos <luisfelipejesussantos123@gmail.com>
2025-06-04 18:26:45 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -47,51 +47,66 @@
      printf ("Vamos cadastrar os dados?\n");
 
      printf ("Digite a letra do primeiro Estado: ");
-     scanf (" %c\n",&estado_A);
-  
+     scanf (" %c",&estado_A);
+     printf ("\n");
+
      printf ("Digite o código da primeira carta: ");
-     scanf ("%s\n",codigo_carta1);
+     scanf ("%s",codigo_carta1);
+     printf ("\n");
 
      printf ("Digite o nome dessa cidade: ");
-     scanf ("%s\n",nome_da_cid_1);
+     scanf ("%s",nome_da_cid_1);
+     printf ("\n");
 
      printf ("Digite a quantidade de habitantes dessa cidade: ");
      scanf ("%lu",&populacao_cid_1);
      printf ("\n");
 
      printf ("Digite a área da cidade em km²: ");
-     scanf ("%f\n",&area_cid_1);
-
-     printf ("Digite o PIB da cidade: ");
-     scanf ("%f\n",&pib_cid_1);
-
-     printf ("Digite o Número de pontos turísticos dessa cidade: ");
-     scanf ("%d\n",&num_pontos_turisticos_cid_1);
+     scanf ("%f",&area_cid_1);
      printf ("\n");
 
+     printf ("Digite o PIB da cidade: ");
+     scanf ("%f",&pib_cid_1);
+     printf ("\n");
+
+     printf ("Digite o Número de pontos turísticos dessa cidade: ");
+     scanf ("%d",&num_pontos_turisticos_cid_1);
+     printf ("\n");
+
+
      printf ("MUITO BEM! Agora vamos cadastrar a segunda Carta\n");
+
      printf ("\n");
 
      printf ("Digite a letra do segundo Estado: ");
-     scanf (" %c\n",&estado_B);
+     scanf (" %c",&estado_B);
+     printf ("\n");
 
      printf ("Digite o código da segunda carta: ");
-     scanf ("%s\n",codigo_carta2);
+     scanf ("%s",codigo_carta2);
+     printf ("\n");
 
      printf ("Digite o nome dessa cidade: ");
-     scanf ("%s\n",nome_da_cid_2);
+     scanf ("%s",nome_da_cid_2);
+     printf ("\n");
 
      printf ("Digite a quantidade de habitantes dessa cidade: ");
-     scanf ("%lu\n",&populacao_cid_2);
+     scanf ("%lu",&populacao_cid_2);
+     printf ("\n");
 
      printf ("Digite a área da cidade em km²: ");
-     scanf ("%f\n",&area_cid_2);
+     scanf ("%f",&area_cid_2);
+     printf ("\n");
 
      printf ("Digite o PIB da cidade: ");
-     scanf ("%f\n",&pib_cid_2);
+     scanf ("%f",&pib_cid_2);
+     printf ("\n");
 
      printf ("Digite o Número de pontos turísticos dessa cidade: ");
-     scanf ("%d\n",&num_pontos_turisticos_cid_2);
+     scanf ("%d",&num_pontos_turisticos_cid_2);
+     printf ("\n");
+
      printf ("\n");
 
      printf ("CADASTRO CONCLUÍDO");
@@ -108,42 +123,66 @@
      printf ("\n");
 
      printf ("Carta 01:\n ");
-     printf ("Estado: %c\n",estado_A);
-     printf (" Código: %s\n",codigo_carta1);
-     printf (" Nome: %s\n",nome_da_cid_1);
-     printf (" População: %lu habitantes\n", populacao_cid_1);
-     printf (" Área (em km²): %f km²\n",area_cid_1);
-     printf (" PIB: %.2f bilhões de reais\n",pib_cid_1);
-     printf (" Nº de pontos turísticos: %d\n", num_pontos_turisticos_cid_1);
-     printf (" Densidade Populacional: %.2f hab./km²\n", densidade1);
-     printf (" PIB per capita de %s: %.2f\n", nome_da_cid_1, pib_per_capita1);
+     printf ("Estado: %c",estado_A);
+     printf ("\n");
+     printf (" Código: %s",codigo_carta1);
+     printf ("\n");
+     printf (" Nome: %s",nome_da_cid_1);
+     printf ("\n");
+     printf (" População: %lu habitantes", populacao_cid_1);
+     printf ("\n");
+     printf (" Área (em km²): %f km²",area_cid_1);
+     printf ("\n");
+     printf (" PIB: %.2f bilhões de reais",pib_cid_1);
+     printf ("\n");
+     printf (" Nº de pontos turísticos: %d", num_pontos_turisticos_cid_1);
+     printf ("\n");
+     printf (" Densidade Populacional: %.2f hab./km²", densidade1);
+     printf ("\n");
+     printf (" PIB per capita de %s: %.2f", nome_da_cid_1, pib_per_capita1);
 
      printf ("\n");
      printf ("\n");
 
     printf ("Carta 02:\n ");
-     printf ("Estado: %c\n",estado_B);
-     printf (" Código: %s\n",codigo_carta2);
-     printf (" Nome: %s\n",nome_da_cid_2);
-     printf (" População: %lu habitantes\n", populacao_cid_2);
-     printf (" Área (em km²): %f km²\n",area_cid_2);
-     printf (" PIB: %.2f bilhões de reais\n",pib_cid_2);
-     printf (" Nº de pontos turísticos: %d\n", num_pontos_turisticos_cid_2);
-     printf (" Densidade Populacional: %.2f hab./km²\n", densidade2);
-     printf (" PIB per capita de %s: %.2f\n", nome_da_cid_2, pib_per_capita2);
+     printf ("Estado: %c",estado_B);
+     printf ("\n");
+     printf (" Código: %s",codigo_carta2);
+     printf ("\n");
+     printf (" Nome: %s",nome_da_cid_2);
+     printf ("\n");
+     printf (" População: %lu habitantes", populacao_cid_2);
+     printf ("\n");
+     printf (" Área (em km²): %f km²",area_cid_2);
+     printf ("\n");
+     printf (" PIB: %.2f bilhões de reais",pib_cid_2);
+     printf ("\n");
+     printf (" Nº de pontos turísticos: %d", num_pontos_turisticos_cid_2);
+     printf ("\n");
+     printf (" Densidade Populacional: %.2f hab./km²", densidade2);
+     printf ("\n");
+     printf (" PIB per capita de %s: %.2f", nome_da_cid_2, pib_per_capita2);
+     printf ("\n");
      printf ("\n");
 
      printf ("COMPARAÇÃO DE CARTAS: \n");
      printf ("CRITÉRIOS: Valor (1) corresponde à primeira carta e Valor (0) corresponde à segunda carta \n");
      printf ("\n");
-     printf ("População: Carta %d venceu\n", populacao_cid_1 > populacao_cid_2);
-     printf ("Área: Carta %d venceu\n", area_cid_1 > area_cid_2);
-     printf ("PIB: Carta %d venceu\n", pib_cid_1 > pib_cid_2);
-     printf ("Pontos Turísticos: Carta %d venceu\n", num_pontos_turisticos_cid_1 > num_pontos_turisticos_cid_2);
-     printf ("Densidade Populacional: Carta %d venceu\n", densidade1 < densidade2);
-     printf ("PIB per capita: Carta %d venceu\n", pib_per_capita1 > pib_per_capita2);
-     printf ("Super Poder: Carta %d venceu\n", SuperPoder1 > SuperPoder2);
+     printf ("População: Carta %d venceu", populacao_cid_1 > populacao_cid_2);
      printf ("\n");
+     printf ("Área: Carta %d venceu", area_cid_1 > area_cid_2);
+     printf ("\n");
+     printf ("PIB: Carta %d venceu", pib_cid_1 > pib_cid_2);
+     printf ("\n");
+     printf ("Pontos Turísticos: Carta %d venceu", num_pontos_turisticos_cid_1 > num_pontos_turisticos_cid_2);
+     printf ("\n");
+     printf ("Densidade Populacional: Carta %d venceu", densidade1 < densidade2);
+     printf ("\n");
+     printf ("PIB per capita: Carta %d venceu", pib_per_capita1 > pib_per_capita2);
+     printf ("\n");
+     printf ("Super Poder: Carta %d venceu", SuperPoder1 > SuperPoder2);
+     printf ("\n");
+
 
      return 0;
 
